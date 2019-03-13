3b6903a75d1529280a3d72a6ff8e90ac65237fa4
takuchantuyoshi <da87435@gmail.com>
2019-03-14 08:36:11 +0900

    👍: 実行時エラーになるのを修正
    why: parseに失敗していたので

--- Sources/TypoChecker/main.swift
+++ Sources/TypoChecker/main.swift
@@ -6,19 +6,22 @@ parser.docString = "typo check"
 let pathOption = StringOption(named: "directoryPath",
                               flag: "directoryPath",
                               required: true,
-                              helpString: "search directoryPath", defaultValue: "")
+                              helpString: "search directoryPath",
+                              defaultValue: "")
 
 let yamlOption = StringOption(named: "ymlPath",
                               flag: "ymlPath",
                               required: false,
-                              helpString: "yaml search rootPath", defaultValue: "")
+                              helpString: "yaml search rootPath",
+                              defaultValue: "")
 
 let reportOption = StringOption(named: "report",
-                              flag: "report",
-                              required: false,
-                              helpString: "output report type", defaultValue: ReportType.json.rawValue)
+                                flag: "report",
+                                required: false,
+                                helpString: "output report type",
+                                defaultValue: ReportType.json.rawValue)
 
-let options: [StringOption] = [pathOption, yamlOption]
+let options: [StringOption] = [pathOption, yamlOption, reportOption]
 
 options.forEach { (option) in
     do {
@@ -40,7 +43,10 @@ let _rootDirectory = parsedArgs?["directoryPath"] as? String ?? ""
 let _yamlDirectory = parsedArgs?["ymlPath"] as? String ?? ""
 let _reportOption = parsedArgs?["report"] as? String ?? ""
 let _reportType = ReportType(rawValue: _reportOption) ?? .json
-let configuration = Configuration(rootDirectory: _rootDirectory,    yamlDirectory: _yamlDirectory, reportType: _reportType)
+
+let configuration = Configuration(rootDirectory: _rootDirectory,
+                                  yamlDirectory: _yamlDirectory,
+                                  reportType: _reportType)
 
 let sourcePath = configuration.rootDirectory
 let ignoredWords: [String] = configuration.ignoredWords
