9048125bff10b9c25aa88c7264c3fe1e24019946
takuchantuyoshi <da87435@gmail.com>
2019-03-14 09:03:55 +0900

    ✏️ : rename TypoCheckerTests

--- Tests/TypoCheckerTests/TypoCheckerTests.swift
+++ Tests/TypoCheckerTests/TypoCheckerTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 import class Foundation.Bundle
 
-final class TypoCheckTests: XCTestCase {
+final class TypoCheckerTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
@@ -12,7 +12,7 @@ final class TypoCheckTests: XCTestCase {
             return
         }
 
-        let fooBinary = productsDirectory.appendingPathComponent("TypoCheck")
+        let fooBinary = productsDirectory.appendingPathComponent("TypoChecker")
 
         let process = Process()
         process.executableURL = fooBinary
