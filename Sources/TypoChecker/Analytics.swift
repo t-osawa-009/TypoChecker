32de790a319e02692a144533ef13596a75f24eca
takuchantuyoshi <da87435@gmail.com>
2019-03-21 19:39:41 +0900

    ✨: filesを使って書き換える
    why: fileを単独で取得できるようにするため

--- Sources/TypoChecker/Analytics.swift
+++ Sources/TypoChecker/Analytics.swift
@@ -6,16 +6,74 @@
 //
 
 import Foundation
+import Files
 
+struct FileObject {
+    let path: String
+    let content: String
+    let contentStrings: [String]
+    let contentNames: [String]
+    
+    init(path: String, content: String) {
+        self.path = path
+        self.content = content
+        self.contentStrings = type(of: self).findStrings(from: content)
+        self.contentNames = type(of: self).findNames(from: content)
+    }
+    
+    private static func findStrings(from line: String) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: "\"[^\"]+\"")
+            let range = NSRange(0..<line.count)
+            let results = regex.matches(in: line, options: .reportCompletion, range: range)
+            return results.compactMap { (line as NSString).substring(with: $0.range) }
+        } catch {
+            return []
+        }
+    }
+    
+    private static func findNames(from fileContents: String) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: "(?<=(^|\\s)(let|var|func|class|enum|struct|protocol)\\s)[a-zA-Z0-9_]+")
+            let range = NSRange(0..<fileContents.count)
+            let results = regex.matches(in: fileContents,
+                                        options: .reportCompletion,
+                                        range: range)
+            return results.compactMap { (fileContents as NSString).substring(with: $0.range) }
+        } catch {
+            return []
+        }
+    }
+}
 
 class Analytics: NSObject {
-    func elementsInEnumerator(atPath path: String) -> [String] {
-        let enumerator = FileManager.default.enumerator(atPath: path)
-        var elements = [String]()
-        while let e = enumerator?.nextObject() as? String {
-            elements.append(e)
+    func elementsInEnumerator(atPath path: String) -> [FileObject] {
+        if let file = try? File(path: path) {
+            if let content = try? file.readAsString(encoding: .utf8) {
+                return [FileObject(path: path, content: content)]
+            } else {
+                return []
+            }
+        } else if let folder = try? Folder(path: path) {
+            var element: [FileObject] = []
+            folder.makeFileSequence(recursive: true, includeHidden: false).forEach { (file) in
+                if let content = try? file.readAsString(encoding: .utf8) {
+                    element.append(FileObject(path: path, content: content))
+                }
+            }
+            return element
+        } else {
+            return []
         }
-        return elements
+    }
+    
+    func findSourcefiles() -> [FileObject] {
+        if !FileManager.default.fileExists(atPath: sourcePath) {
+            print("Invalid configuration: \(sourcePath) does not exist.")
+            exit(1)
+        }
+        
+        return elementsInEnumerator(atPath: sourcePath)
     }
     
     func findLineNumber(from fileContents: String, targetWord: String) -> Int? {
